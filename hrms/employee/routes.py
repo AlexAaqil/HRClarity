f05424afd2b0f001fc15d0ea3fdc43f1999a0820
alexaaqil <alexdequantes@gmail.com>
2023-09-06 23:40:33 +0300

    Added functionality to login users using their email and national_id as their password

--- hrms/employee/routes.py
+++ hrms/employee/routes.py
@@ -1,5 +1,8 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, url_for, redirect, flash
+from flask_login import login_user
+from hrms import bcrypt
 from hrms.employee.forms import Login
+from hrms.models import Employee
 
 
 employee = Blueprint('employee', __name__)
@@ -8,4 +11,12 @@ employee = Blueprint('employee', __name__)
 @employee.route('/employee/login', methods=['GET', 'POST'])
 def login():
     form = Login()
+    if form.validate_on_submit():
+        employee = Employee.query.filter_by(email_address=form.email_address.data).first()
+        if employee and form.password.data == str(employee.national_id):
+            login_user(employee)
+            flash('Logged in successfully!', 'success')
+            return redirect(url_for('main.home'))
+        else:
+            flash('Oops! Check your credentials!', 'danger')
     return render_template('login.html', page_title='Employee Login', form=form)
